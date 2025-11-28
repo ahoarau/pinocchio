189ba868aa458d98e42a7238e97892d4d88182aa
Antoine Hoarau <703240+ahoarau@users.noreply.github.com>
2025-11-28 17:47:03 +0100

    change mimic test

--- unittest/algo-check.cpp
+++ unittest/algo-check.cpp
@@ -53,10 +53,78 @@ BOOST_AUTO_TEST_CASE(test_check)
   BOOST_CHECK(!model.check(ABAChecker())); // some inertias are negative ... check fail.
 
   pinocchio::Model model_mimic;
-  buildModels::humanoidRandom(model_mimic, false, true);
+  const bool using_free_flyer = true;
+  const bool using_mimic = true;
+  buildModels::humanoidRandom(model_mimic, using_free_flyer, using_mimic);
   pinocchio::Data data_mimic(model_mimic);
-  BOOST_CHECK(!model_mimic.check(MimicChecker()));
+  BOOST_CHECK(model_mimic.check(MimicChecker()) == false);
   BOOST_CHECK(model_mimic.check(data_mimic));
+}
+
+BOOST_AUTO_TEST_CASE(test_mimic_check)
+{
+  {
+    Model manipulator_no_mimic;
+    const bool using_mimic = false;
+    buildModels::manipulator(manipulator_no_mimic, using_mimic);
+    BOOST_CHECK(manipulator_no_mimic.mimicked_joints.size() == 0);
+    BOOST_CHECK(manipulator_no_mimic.mimicking_joints.size() == 0);
+    BOOST_CHECK(manipulator_no_mimic.check(MimicChecker()) == true);
+  }
+  {
+    Model manipulator_mimic;
+    const bool using_mimic = true;
+    buildModels::manipulator(manipulator_mimic, using_mimic);
+    BOOST_CHECK(manipulator_mimic.mimicked_joints.size() > 0);
+    BOOST_CHECK(manipulator_mimic.mimicking_joints.size() > 0);
+    BOOST_CHECK(manipulator_mimic.check(MimicChecker()) == false);
+  }
+  {
+    Model humanoid_ff_no_mimic;
+    const bool using_free_flyer = true;
+    const bool using_mimic = false;
+    buildModels::humanoidRandom(humanoid_ff_no_mimic, using_free_flyer, using_mimic);
+    BOOST_CHECK(humanoid_ff_no_mimic.mimicked_joints.size() == 0);
+    BOOST_CHECK(humanoid_ff_no_mimic.mimicking_joints.size() == 0);
+    BOOST_CHECK(humanoid_ff_no_mimic.check(MimicChecker()) == true);
+  }
+  {
+    Model humanoid_ff_mimic;
+    const bool using_free_flyer = true;
+    const bool using_mimic = true;
+    buildModels::humanoidRandom(humanoid_ff_mimic, using_free_flyer, using_mimic);
+    BOOST_CHECK(humanoid_ff_mimic.mimicked_joints.size() > 0);
+    BOOST_CHECK(humanoid_ff_mimic.mimicking_joints.size() > 0);
+    BOOST_CHECK(humanoid_ff_mimic.check(MimicChecker()) == false);
+  }
+  {
+    Model humanoid_no_ff_no_mimic;
+    const bool using_free_flyer = false;
+    const bool using_mimic = false;
+    buildModels::humanoidRandom(humanoid_no_ff_no_mimic, using_free_flyer, using_mimic);
+    BOOST_CHECK(humanoid_no_ff_no_mimic.mimicked_joints.size() == 0);
+    BOOST_CHECK(humanoid_no_ff_no_mimic.mimicking_joints.size() == 0);
+    BOOST_CHECK(humanoid_no_ff_no_mimic.check(MimicChecker()) == true);
+  }
+  {
+    Model humanoid_no_ff_mimic;
+    const bool using_free_flyer = false;
+    const bool using_mimic = true;
+    buildModels::humanoidRandom(humanoid_no_ff_mimic, using_free_flyer, using_mimic);
+    BOOST_CHECK(humanoid_no_ff_mimic.mimicked_joints.size() > 0);
+    BOOST_CHECK(humanoid_no_ff_mimic.mimicking_joints.size() > 0);
+    BOOST_CHECK(humanoid_no_ff_mimic.check(MimicChecker()) == false);
+  }
+  {
+    Model custom_model;
+    custom_model.addJoint(0, JointModelRX(), SE3::Identity(), "j1");
+    custom_model.addJoint(1, JointModelRX(), SE3::Identity(), "j2");
+    custom_model.addJoint(
+      2, JointModelMimic(JointModelRX(), custom_model.joints[1], 1., 0.), SE3::Identity(), "j3");
+    BOOST_CHECK(custom_model.mimicked_joints.size() > 0);
+    BOOST_CHECK(custom_model.mimicking_joints.size() > 0);
+    BOOST_CHECK(custom_model.check(MimicChecker()) == false);
+  }
 }
 
 BOOST_AUTO_TEST_SUITE_END()
